deb5977c4317ad085c9c3bfab3f7f30b3820024f
BenjaminLangenaken <benjamin.langenaken@cronos.be>
2022-08-04 11:54:27 +0200

    Add second filter (warm/cold)

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,51 +7,78 @@ import MealsFilter from './MealsFilter';
 const DUMMY_MEALS = [
 	{
 		id: 'm1',
-		name: 'Sushi sashimi bowl',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-		type: 'Vegetarian',
+		name: 'Carpaccio',
+		description: 'Classic Italian starter',
+		price: 17.99,
+		type: 'Non-Vegetarian',
+		warmth: 'Cold',
 	},
 	{
 		id: 'm2',
-		name: 'Saltimbocca alla Romana',
-		description: 'An Italian classic!',
-		price: 28.5,
-		type: 'Non-Vegetarian',
+		name: 'Sushi sashimi bowl',
+		description: 'Finest fish and veggies',
+		price: 19.99,
+		type: 'Vegetarian',
+		warmth: 'Cold',
 	},
 	{
 		id: 'm3',
-		name: 'BBQ Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
+		name: 'Saltimbocca alla Romana',
+		description: 'Roman-style veal cutlets',
+		price: 28.5,
 		type: 'Non-Vegetarian',
+		warmth: 'Hot',
 	},
 	{
 		id: 'm4',
+		name: 'BBQ Burger',
+		description: 'American, raw, meaty',
+		price: 14.99,
+		type: 'Non-Vegetarian',
+		warmth: 'Hot',
+	},
+	{
+		id: 'm5',
 		name: 'Green Poké',
 		description: 'Healthy vegetarian dish',
-		price: 14.99,
+		price: 11.99,
 		type: 'Vegetarian',
+		warmth: 'Cold',
 	},
 ];
 
 const AvailableMeals = () => {
-	const [filteredType, setFilteredType] = useState('All');
+	const [filteredType, setFilteredType] = useState('All Types');
+	const [filteredWarmth, setFilteredWarmth] = useState('Hot and Cold');
 
-	const filterSubmitHandler = (selectedType) => {
+	const filterTypeSubmitHandler = (selectedType) => {
 		setFilteredType(selectedType);
 	};
 
+	const filterWarmthSubmitHandler = (selectedWarmth) => {
+		setFilteredWarmth(selectedWarmth);
+	};
+
 	let filteredMeals;
-	if (filteredType === 'All') {
+	if (filteredType === 'All Types' && filteredWarmth === 'Hot and Cold') {
 		filteredMeals = DUMMY_MEALS;
-	} else {
+	} else if (filteredType === 'All Types') {
+		filteredMeals = DUMMY_MEALS.filter((meal) => {
+			return meal.warmth === filteredWarmth;
+		});
+	} else if (filteredWarmth === 'Hot and Cold') {
 		filteredMeals = DUMMY_MEALS.filter((meal) => {
 			return meal.type === filteredType;
+		});
+	} else {
+		filteredMeals = DUMMY_MEALS;
+		filteredMeals = DUMMY_MEALS.filter((meal) => {
+			return meal.type === filteredType && meal.warmth === filteredWarmth;
 		});
 	}
 
 	const mealsList = filteredMeals.map((meal) => (
+		// const mealsList = DUMMY_MEALS.map((meal) => (
 		<MealItem
 			key={meal.id}
 			id={meal.id}
@@ -59,14 +86,17 @@ const AvailableMeals = () => {
 			description={meal.description}
 			price={meal.price}
 			type={meal.type}
+			warmth={meal.warmth}
 		/>
 	));
 	return (
 		<section className={classes.meals}>
 			<Card>
 				<MealsFilter
-					onFilterSubmit={filterSubmitHandler}
+					onFilterTypeSubmit={filterTypeSubmitHandler}
 					filteredFoodType={filteredType}
+					onFilterWarmthSubmit={filterWarmthSubmitHandler}
+					filteredFoodWarmth={filteredWarmth}
 				/>
 				<ul>{mealsList}</ul>
 			</Card>
--- src/components/Meals/MealsFilter.js
+++ src/components/Meals/MealsFilter.js
@@ -1,20 +1,52 @@
 import classes from './MealsFilter.module.css';
 
 const MealsFilter = (props) => {
-	const filterHandler = (event) => {
-		props.onFilterSubmit(event.target.value);
+	const filterTypeHandler = (event) => {
+		props.onFilterTypeSubmit(event.target.value);
+	};
+
+	const filterWarmthHandler = (event) => {
+		props.onFilterWarmthSubmit(event.target.value);
 	};
 
 	return (
-		<div className={classes['meals-filter']}>
-			<div className={classes['meals-filter__control']}>
-				<label>Filter by type</label>
-				<select onChange={filterHandler} value={props.filteredFoodType}>
-					<option value="All">All</option>
+		<div className={classes.meals__filter}>
+			<div className={classes['filter-section']}>
+				<h3>Diet</h3>
+				<select
+					onChange={filterTypeHandler}
+					value={props.filteredFoodType}
+				>
+					<option value="All Types">All Types</option>
 					<option value="Vegetarian">Vegetarian</option>
 					<option value="Non-Vegetarian">Non-Vegetarian</option>
 				</select>
 			</div>
+
+			<div className={classes['filter-section']}>
+				<h3>Hot or Cold?</h3>
+				<select
+					onChange={filterWarmthHandler}
+					value={props.filteredFoodWarmth}
+				>
+					<option value="Hot and Cold">Hot and Cold</option>
+					<option value="Hot">Hot</option>
+					<option value="Cold">Cold</option>
+				</select>
+			</div>
+
+			{/* <div className={classes['filter-section']}>
+				<h3>Ingredients</h3>
+				<div>
+					<button>Beef</button>
+					<button>Rice</button>
+					<button>Salmon</button>
+					<button>Veal</button>
+					<button>Prosciutto</button>
+					<button>Onion</button>
+					<button>Spinach</button>
+				</div>
+			</div> */}
 		</div>
 	);
 };
